947a6b4d5a10274025994790fe576a2b256b48de
AnabelSalomone <anabel.salomone@gmail.com>
2020-08-15 12:36:01 +0200

    checked keys

--- src/my_bot.py
+++ src/my_bot.py
@@ -12,3 +12,13 @@ auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
 API = tweepy.API(auth)
 
 get_mentions = API.mentions_timeline()
+
+# trick to get a dict from the class obj
+# get_mentions[0].__dict__.keys()
+# I personally checked for a a JSON validator
+
+for mention in get_mentions:
+    print(mention.text)
+    print(mention.user.screen_name)
+
+
