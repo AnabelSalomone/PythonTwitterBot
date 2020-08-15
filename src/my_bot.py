d99abb760e5aa88fa90acd8679fc480b1f787b67
AnabelSalomone <anabel.salomone@gmail.com>
2020-08-15 22:00:08 +0200

    v1 finished

--- src/my_bot.py
+++ src/my_bot.py
@@ -15,10 +15,31 @@ get_mentions = API.mentions_timeline()
 
 # trick to get a dict from the class obj
 # get_mentions[0].__dict__.keys()
-# I personally checked for a a JSON validator
 
-for mention in get_mentions:
-    print(mention.text)
-    print(mention.user.screen_name)
+FILE_LAST_SEEN_ID = "last_seen_id.txt"
+
+def retrive_last_seen_id(filename):
+    file_read = open(FILE_LAST_SEEN_ID, "r")
+    last_seen_id = int(file_read.read().strip())
+    file_read.close()
+    return last_seen_id
+
+def store_last_seen_id(last_seen_id, filename):
+    file_write = open(FILE_LAST_SEEN_ID, "w")
+    file_write.write(str(last_seen_id))
+    file_write.close()
+    return
+
+last_seen_id = retrive_last_seen_id(FILE_LAST_SEEN_ID)
+
+mentions = API.mentions_timeline(last_seen_id)
+
+for tweet in reversed(mentions):
+    print(tweet.user.screen_name)
+    last_seen_id = tweet.id
+    store_last_seen_id(last_seen_id, FILE_LAST_SEEN_ID)
+    if 'alejoferre' in tweet.user.screen_name.lower():
+        print("worked")
+        API.update_status("@" + tweet.user.screen_name + " Esto es un tweet bot, estoy haciendo un test en python :)")
 
 
